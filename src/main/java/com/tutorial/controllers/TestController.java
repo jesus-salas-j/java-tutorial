7d4e5e2153728d828d2f64199378e4cfd1bfadb0
jesus-salas-j <jesus.salas@schibsted.com>
2019-03-17 18:28:40 +0100

    Refactor

--- src/main/java/com/tutorial/controllers/TestController.java
+++ src/main/java/com/tutorial/controllers/TestController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class TestController {
@@ -30,34 +30,31 @@ public class TestController {
     }
 
     @RequestMapping("/all")
-    public String all() {
-        return "";
+    public ResponseEntity all() {
+        return new ResponseEntity("Products: " + StringListFrom(products), HttpStatus.CREATED);
     }
 
     @RequestMapping(value="/new", method=RequestMethod.POST)
     public ResponseEntity createProduct(@RequestBody String product) {
-        String productsString = "";
-
+        product = formattedInputOf(product);
         products.add(product);
-
-        productsString = products.stream()
-                .reduce((x, y) -> x + "\n" + y)
-                .toString();
-
-        return new ResponseEntity("Products: " + productsString, HttpStatus.CREATED);
+        return new ResponseEntity("Products: " + StringListFrom(products), HttpStatus.CREATED);
     }
 
     @RequestMapping(value="/delete", method=RequestMethod.DELETE)
     public ResponseEntity deleteProduct(@RequestBody String product) {
-        String productsString = "";
-
+        product = formattedInputOf(product);
         products.remove(product);
+        return new ResponseEntity("Products: " + StringListFrom(products), HttpStatus.CREATED);
+    }
 
-        productsString = products.stream()
-                .reduce((x, y) -> x + "\n" + y)
-                .toString();
+    private String StringListFrom(List<String> products) {
+        return products.stream()
+            .collect(Collectors.joining(" "));
+    }
 
-        return new ResponseEntity("Products: " + productsString, HttpStatus.CREATED);
+    private String formattedInputOf(@RequestBody String product) {
+        return product.replaceAll("[=]", "");
     }
 }
 
