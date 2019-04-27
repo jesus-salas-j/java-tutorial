8d86bd8a37d74117dd48f06ffa3a0022b6578fa7
jesus-salas-j <jesus.salas@schibsted.com>
2019-04-27 23:12:20 +0200

    Added command line runner

--- src/main/java/com/tutorial/TutorialJavaApplication.java
+++ src/main/java/com/tutorial/TutorialJavaApplication.java
@@ -1,14 +1,23 @@
 package com.tutorial;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class TutorialJavaApplication {
+public class TutorialJavaApplication implements CommandLineRunner {
 
     public static void main (String[] args) {
+
         SpringApplication.run(TutorialJavaApplication.class, args);
     }
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        System.out.println("Hello world from Command Line Runner");
+    }
 }
+
+
